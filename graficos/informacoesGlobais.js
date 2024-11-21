ccdccc051edae0e4a6cea8fd04b957aae4a74eb0
gislainecrestani <107581676+gislainecrestani@users.noreply.github.com>
2024-11-21 16:45:06 -0300

    21/11

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,1 +1,8 @@
-const url = 'https://raw.githubusercontent.com/TamirisCeolin/ciencia-de-dados-3C/refs/heads/main/dados-globais.json';
+const url = 'https://raw.githubusercontent.com/gislainecrestani/ciencia-dados-gislaine/refs/heads/main/dados-globais.json';
+
+async function visualizadorInformacoesGlobais() {
+    constres = await fetch (url);
+
+    const dados = await resizeBy.json();
+    
+}
