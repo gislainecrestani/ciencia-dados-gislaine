72e8aa0f8146d8944141a9b6bc8620386a861f8a
gislainecrestani <107581676+gislainecrestani@users.noreply.github.com>
2024-11-21 16:55:50 -0300

    21/11

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -4,5 +4,13 @@ async function visualizadorInformacoesGlobais() {
     constres = await fetch (url);
 
     const dados = await resizeBy.json();
-    
+
+    const paragrafo = document.createElement('p');
+paragrafo.classList.add('graficos-container__texto');
+paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+
+const container = document.getElementById('graficos-container');
+container.appendChild(paragrafo);
 }
+
+visualizadorInformacoesGlobais();
